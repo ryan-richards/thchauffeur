1f6188c412d1b2006ddb8d1a768bef899e783920
Anna Rowan <annarowan@Annas-MacBook-Pro.local>
2021-08-19 21:15:13 +0100

    first commit

---
+++ src/components/Landing.vue
@@ -0,0 +1,47 @@
+<template>
+
+<div class="container">
+<div class="card-under has-text-left mb-6">
+        <div class="title ">TH Chauffeur Service</div>
+         <div class="subtitle">Under Construction</div>
+         <div class="block">
+           <p class="has-text-black">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius ad ipsam unde nisi exercitationem quasi sint esse amet eaque optio mollitia dolorum iste corporis, commodi qui quo enim reprehenderit quisquam quaerat deleniti aliquam cupiditate. Odit vel amet, nam necessitatibus optio nisi illum expedita officia voluptatem perferendis magnam iusto laudantium rerum, enim minus recusandae beatae fugit quis minima? Eligendi, molestiae. Accusamus impedit ea tempora nam maxime, odio deserunt necessitatibus libero! Voluptates laudantium unde maxime incidunt. Doloribus nemo illum eligendi animi soluta veniam libero perspiciatis suscipit, perferendis excepturi molestiae porro ullam atque at natus esse ducimus ut officia odio, consequatur quod voluptates?</p>
+         </div>
+         <div class="buttons">
+           <a href="mailto: terryhutley@me.com" class="button is-outline is-medium">Email</a>
+           <a href="tel: +447595023157" class="button is-medium">Mobile</a>
+         </div>
+</div>
+</div>
+
+
+  
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+  .card-under{
+     backdrop-filter: blur(17px) saturate(169%);
+    -webkit-backdrop-filter: blur(17px) saturate(169%);
+    background-color: rgba(255, 255, 255, 0.62);
+    border-radius: 12px;
+    border: 1px solid rgba(209, 213, 219, 0.3);
+    padding: 2rem;
+}
+
+.button {
+       backdrop-filter: blur(0px) saturate(169%);
+    -webkit-backdrop-filter: blur(17px) saturate(169%);
+    background-color: rgba(255, 255, 255, 0);
+    border-radius: 12px;
+    border: 1px solid rgba(23, 61, 119, 0.3);
+}
+
+
+
+
+</style>
