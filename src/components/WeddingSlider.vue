0a5f86e5dcc973d3999851c951ca92376d8cbd9f
ryan <ryan.richards@phasora.net>
2022-05-05 16:24:16 +0100

    Cloudinary

--- src/components/WeddingSlider.vue
+++ src/components/WeddingSlider.vue
@@ -3,23 +3,23 @@
       <n-carousel show-arrow>
         <img
           class="carousel-img"
-          src="../assets/Pic14.jpg"
+          src="https://res.cloudinary.com/dvihzm2ok/image/upload/v1651764183/terryhutelysite/20e3be6e-54c6-4b67-af48-0a0cf199d5c5_hlynea.jpg"
         />
         <img
           class="carousel-img"
-          src="../assets/Pic13.jpg"
+          src="https://res.cloudinary.com/dvihzm2ok/image/upload/v1651764185/terryhutelysite/4bada218-a68d-43f3-bac8-d162f71ba82a_vxhzfg.jpg"
         />
         <img
           class="carousel-img"
-          src="../assets/Pic15.jpg"
+          src="https://res.cloudinary.com/dvihzm2ok/image/upload/v1651764183/terryhutelysite/271ed5c3-28d0-430d-a487-6a331bfec8a4_utlcmu.jpg"
         />
         <img
           class="carousel-img"
-          src="../assets/Pic16.jpg"
+          src="https://res.cloudinary.com/dvihzm2ok/image/upload/v1651764185/terryhutelysite/b13760f9-fb2e-407b-a52f-6020022f015b_scumfz.jpg"
         />
         <img
           class="carousel-img"
-          src="../assets/Pic17.jpg"
+          src="https://res.cloudinary.com/dvihzm2ok/image/upload/v1651764183/terryhutelysite/f0e2b5d0-4775-4e3b-8abf-b26b5ce630b3_sela7l.jpg"
         />
       </n-carousel>
     </n-space>
